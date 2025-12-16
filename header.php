1d9eb1824fcff13e8ab542132b863ec7b3a4bf97
funachandesu <dafunafuna@icloud.com>
2025-12-17 05:15:16 +0900

    Metaピクセルの設置

--- header.php
+++ header.php
@@ -55,6 +55,25 @@
     <!-- End Google Tag Manager -->
     <link rel="stylesheet" href="https://use.typekit.net/xcg6uqz.css">
     <?php wp_head(); ?>
+
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '2256097438134253');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=2256097438134253&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+
 </head>
 <?php if (is_user_logged_in()) : ?>
     <style type="text/css">
